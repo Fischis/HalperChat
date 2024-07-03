58962c4226615aa3147a09b8d1c3e2718e975ffe
Fischis <90457329+Fischis@users.noreply.github.com>
2024-07-03 12:28:42 +0000

    secrets update

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ from langchain import VectorDBQA, VectorDBQAWithSourcesChain
 st.title('🦜🔗 Hilfe Hilfe')
 
 #openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-openai_api_key = st.secrets(["openai_api_key"])
+openai_api_key = st.secrets["openai_api_key"]
 if 'vStore' not in st.session_state:
     st.session_state.vStore = None
 
